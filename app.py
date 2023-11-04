063ef5b4fb9971a39be21c7dfc229d86b7d22e5b
RushikesH-Bakshi <rushikeshbakshi2301@gmail.com>
2023-11-04 19:33:37 +0530

    Final commit

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 from flask import Flask, render_template, request
+import os
 import jsonify
 import requests
 import pickle
 import numpy as np
 import sklearn
 
-app = Flask(__name__)
+TEMPLATE_DIR = os.path.abspath("./templates")
+STATIC_DIR = os.path.abspath("./static")
+
+app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
 model = pickle.load(open("ParkinsonsDiseaseModel.pkl", "rb"))
 print("Model loaded successfully")
+
 
 @app.route("/", methods=["GET", "POST"])
 def Home():
     return render_template("index.html")
+
 
 @app.route("/predict", methods=["POST", "GET"])
 def predict():
@@ -39,13 +45,44 @@ def predict():
         D2 = float(request.form["D2"])
         PPE = float(request.form["PPE"])
 
-        predict = model.predict([[MDVP_F0, MDVP_Fhi, MDVP_Flo, MDVP_Jitter_per, MDVP_Jitter_abs, MDVP_RAP, MDVP_PPQ, Jitter_DDP, MDVP_Shimmer, MDVP_Shimmer_dB, Shimmer_APQ3, Shimmer_APQ5, MDVP_APQ, Shimmer_DDA, NHR, HNR, RPDE, DFA, spread1, spread2, D2, PPE]])
-        
+        predict = model.predict(
+            [
+                [
+                    MDVP_F0,
+                    MDVP_Fhi,
+                    MDVP_Flo,
+                    MDVP_Jitter_per,
+                    MDVP_Jitter_abs,
+                    MDVP_RAP,
+                    MDVP_PPQ,
+                    Jitter_DDP,
+                    MDVP_Shimmer,
+                    MDVP_Shimmer_dB,
+                    Shimmer_APQ3,
+                    Shimmer_APQ5,
+                    MDVP_APQ,
+                    Shimmer_DDA,
+                    NHR,
+                    HNR,
+                    RPDE,
+                    DFA,
+                    spread1,
+                    spread2,
+                    D2,
+                    PPE,
+                ]
+            ]
+        )
+
         output = round(predict[0])
         if output == 0:
-            return render_template("index.html", prediction_text="NO DETECTION of Parkinson's Disease")
+            return render_template(
+                "index.html", prediction_text="NO DETECTION of Parkinson's Disease"
+            )
         else:
-            return render_template("index.html", prediction_text="Parkinson's Disease DETECTED")
+            return render_template(
+                "index.html", prediction_text="Parkinson's Disease DETECTED"
+            )
     else:
         return render_template("index.html")
 
